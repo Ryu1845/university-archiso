081b7d414a67de1438e2a64b04aead6005aa8ca4
Ryu18 <ryu@tpgjbo.xyz>
2021-09-18 15:58:43 +0200

    modified profiledef

--- profiledef.sh
+++ profiledef.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
+iso_name="ryu-university"
+iso_label="UNI_RYU_$(date +%Y%m)"
+iso_publisher="Ryu1845 <ryu@tpgjbo.xyz>"
+iso_application="University Math-oriented"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
